3df5a9f6f136328f3a0b0b6162250b0e0a177ff0
shantik998 <shantik998@gmail.com>
2023-01-31 13:11:19 -0600

    added code

---
+++ user_tuples_etc_out.py
@@ -0,0 +1,26 @@
+ /usr/bin/env /usr/local/bin/python3 /Users/shantikandel/.vscode/extensions/ms-python.python-2023.1.10091012/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 65117 -- /Users/shantikandel/Desktop/datafun\ project3/datafun-03-datatypes/user_tuples_etc.py 
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Shantis-MacBook-Pro:datafun-03-datatypes shantikandel$  /usr/bin/env /usr/local/bin/python3 /Users/shantikandel/.vscode/extensions/ms-python.python-2023.1.10091012/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 65117 -- /Users/shantikandel/Desktop/datafun\ project3/datafun-03-datatypes/user_tuples_etc.py 
+height_tuple = (1, 2, 3, 4, 5)
+weight_tuple = (100, 200, 300, 400, 500)
+tupleCat = (1, 2, 3, 4, 5, 100, 200, 300, 400, 500)
+tupleAThrice = (1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+True
+False
+((1, 2, 3, 4, 5), (100, 200, 300, 400, 500))
+(2, 3, 4)
+(100, 200, 300)
+(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+(100, 200, 300, 400, 500, 100, 200, 300, 400, 500)
+3
+300
+(1, 2, 3, 4, 5, 100, 200, 300, 400, 500)
+Set 1: {'Lamborghini', 'McLaren', 'Ferrari', 'Porsche'}
+Set 2: {'Range Rover', 'Jeep Grand Cherokee', 'BMW X5', 'Toyota Land Cruiser'}
+4
+True
+True
+(base) Shantis-MacBook-Pro:datafun-03-datatypes shantikandel$ 
