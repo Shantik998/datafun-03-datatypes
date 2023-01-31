5fc74cc3b08c9a204131e1e27241b3db9c0cc330
shantik998 <shantik998@gmail.com>
2023-01-31 13:22:25 -0600

    Completed Project

---
+++ xtra_p3_out.py
@@ -0,0 +1,26 @@
+ /usr/bin/env /usr/local/bin/python3 /Users/shantikandel/.vscode/extensions/ms-python.python-2023.1.10091012/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 49153 -- /Users/shantikandel/Desktop/datafun\ project3/datafun-03-datatypes/xtra_p3.py 
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Shantis-MacBook-Pro:datafun-03-datatypes shantikandel$  /usr/bin/env /usr/local/bin/python3 /Users/shantikandel/.vscode/extensions/ms-python.python-2023.1.10091012/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 49153 -- /Users/shantikandel/Desktop/datafun\ project3/datafun-03-datatypes/xtra_p3.py 
+415
+197
+13
+
+sorted(longwords) = ['conference.', 'conscience;', 'consequence;', 'constantly.', 'corruption,', 'countenance', 'countenance,', 'enterprise,', 'honourable.', 'immediately.', 'replication', 'themselves,', 'threatening']
+
+TESTING...if nothing prints before the testing is done, you passed!
+
+PYDEV DEBUGGER WARNING:
+sys.settrace() should not be used when the debugger is being used.
+This may cause the debugger to stop working correctly.
+If this is needed, please check: 
+http://pydev.blogspot.com/2007/06/why-cant-pydev-debugger-work-with.html
+to see how to restore the debug tracing back correctly.
+Call Location:
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/doctest.py", line 1501, in run
+    sys.settrace(save_trace)
+
+TESTING DONE
+(base) Shantis-MacBook-Pro:datafun-03-datatypes shantikandel$ 
